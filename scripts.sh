f218656bbb38bd2305825a5f25483e7719a9b7af
asvaaron <asvaaron@gmail,com>
2024-09-22 14:47:51 -0600

    Add new line to script
    Ad new line for renaiming the data correctly

--- scripts.sh
+++ scripts.sh
@@ -3,6 +3,12 @@
 cd /content/drive/MyDrive/TEC/Tesis-TEC/POSTER/
 
 
+# CHANGE name of files
+for f in *.jpg
+do
+    mv -v "${f}" "${f%.*}_aligned.${f##*.}"
+done
+
 # Google Collab Location
 nohup python train.py --gpu 0 --batch_size 125 --dataset rafdb --epochs 250 > training.log &
 
