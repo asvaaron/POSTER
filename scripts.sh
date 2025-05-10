e7ab3fa4af3de456d8afd961a94150ebd2c92a3b
asvaaron <asvaaron@gmail,com>
2025-05-09 20:04:29 -0600

    Changes in scripts file
    Add  command to run all model sizes

--- scripts.sh
+++ scripts.sh
@@ -1,16 +1,25 @@
 
 # Path Location Google Collab
-cd /content/drive/MyDrive/TEC/Tesis-TEC/CODE/POSTER/
+cd /content/drive/MyDrive/TEC/Tesis-TEC/CODE/POSTER_Aaron
 
-
-# CHANGE name of files
-for f in *.jpg
-do
-    mv -v "${f}" "${f%.*}_aligned.${f##*.}"
-done
 
 # Google Collab Location
-nohup python train.py --gpu 0 --batch_size 110 --dataset rafdb --epochs 250 > training.log &
+# Training multiple models in parallel with nohup
+
+# Train base model without weights
+nohup python train.py --gpu 0 --batch_size 100 --dataset rafdb --modeltype base --epochs 250 > training_base_no_weights.log 2>&1 &
+
+# Train large model without weights
+nohup python train.py --gpu 0 --batch_size 120 --dataset rafdb --modeltype large --epochs 250 > training_large_no_weights.log 2>&1 &
+
+# Train base model with pretrained weights
+nohup python train.py --gpu 0 --batch_size 100 --dataset rafdb --checkpoint /models/weights.ph --modeltype base --epochs 250 > training_base_weights.log 2>&1 &
+
+# Wait for all processes to complete
+wait
+
 
 # LOCAL
 nohup python train.py --gpu 0 --batch_size 95 --dataset rafdb --epochs 250 > training.log &
+
+nohup python train.py --gpu 0,1 --batch_size 150 --dataset rafdb --epochs 250 > training.log &
