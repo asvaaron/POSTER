6e5173122fdb6d157a8aff0ab0db04b934123fac
asvaaron <asvaaron@gmail,com>
2025-08-29 21:18:37 -0600

    Changes to script
    Add code for running in local with multiple gpus
    Add code for testing codes in large models

--- scripts.sh
+++ scripts.sh
@@ -27,14 +27,25 @@ wait
 # LOCAL
 nohup python train.py --gpu 0 --batch_size 95 --dataset rafdb --epochs 250 > training.log &
 
-nohup python train.py --gpu 0,1 --batch_size 150 --dataset rafdb --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 150 --dataset rafdb --epochs 250 --checkpoint epoch10_acc0.9169.pth > training.log &
+
+nohup python train.py --gpu 0,1 --batch_size 150 --dataset rafdb --modeltype base --epochs 250 --checkpoint checkpoint/epoch81_acc0.9234.pth >> training.log &
 
 
 ## Test Dataset
 
 
 # Test small model
-python test.py --gpu 0 --batch_size 100 --dataset rafdb --modeltype small --checkpoint checkpoint/training_small_no_changes/epoch187_acc0.9237.pth -p
+python test.py --gpu 0,1 --batch_size 100 --dataset rafdb --modeltype small --checkpoint checkpoint/training_small_no_changes/epoch187_acc0.9237.pth -p
 
 python test.py --gpu 0 --batch_size 100 --dataset rafdb --modeltype small --checkpoint checkpoint/training_small_no_changes/epoch187_acc0.9237.pth
 
+# Test base model
+
+python test.py --gpu 0,1 --batch_size 100 --dataset rafdb --modeltype base --checkpoint checkpoint/training_base_no_changes/epoch97_acc0.9244.pth -p
+
+python test.py --gpu 0 --batch_size 100 --dataset rafdb --modeltype base --checkpoint checkpoint/training_base_no_changes/epoch97_acc0.9244.pth
+
+# Test Large model
+
+python test.py --gpu 0 --batch_size 100 --dataset rafdb --modeltype large --checkpoint checkpoint/training_large_no_changes/epoch202_acc0.9234.pth -p
