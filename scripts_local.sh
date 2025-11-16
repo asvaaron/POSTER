9191efa9458a1f4b03aa3a25b15bba1eb6c3629a
aaron <asvaaron@gmail.com>
2025-11-16 17:56:00 -0600

    Add new script for training
    Add new entries with model training sizes and head type

--- scripts_local.sh
+++ scripts_local.sh
@@ -1,16 +1,28 @@
 # Training
 
-nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --epochs 250 > training.log &
-nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --epochs 250 > training.log &
-nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype small --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type simple --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --head_type simple --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype small  --head_type simple --epochs 250 > training.log &
+
+
+
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type enhanced_v1 --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --head_type enhanced_v1 --epochs 250 > training.log &
+nohup python train.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype small --head_type enhanced_v1 --epochs 250 > training.log &
+
 
 # Testing
 python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --head_type simple --checkpoint checkpoint/local_base/epoch162_acc0.9254.pth -p
 python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type simple --checkpoint checkpoint/training_large_no_changes/epoch205_acc0.9241.pth -p
+python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype small --head_type simple --checkpoint checkpoint/training_small_no_changes/epoch187_acc0.9237.pth -p
 
 python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --head_type enhanced_v1 --checkpoint checkpoint/local_base_enhanced_v1/epoch219_acc0.9247.pth -p
+python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type enhanced_v1 --checkpoint checkpoint/local_large_enchanced_v1/epoch187_acc0.9254.pth -p
+python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype small --head_type enhanced_v1 --checkpoint checkpoint/local_small_enhanced_v1/epoch191_acc0.9234.pth -p
 
 
 python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype base --head_type enhanced_v2 --checkpoint checkpoint/local_base_enhanced_v2/epoch58_acc0.9221.pth -p
 
 python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type enhanced_v3 --checkpoint checkpoint/local_base_enhanced_v3/epoch227_acc0.9254.pth -p
+
+python test.py --gpu 0,1 --batch_size 120 --dataset rafdb --modeltype large --head_type enhanced_v7 --checkpoint checkpoint/local_large_enhanced_v7/epoch71_acc0.9244.pth -p
