fa6d81c352efe5b9b072187202c0b2b512f0d610
aaron <asvaaron@gmail.com>
2026-01-02 22:00:16 -0600

    Make changes in the log reader
    Convert parser into  a method and return three of the main values

--- log_reader.py
+++ log_reader.py
@@ -1,24 +1,33 @@
 import re
 import matplotlib.pyplot as plt
 
-epochs = []
-train_loss = []
-val_loss = []
+
+
+
+
+def parse_log(log_file):
+
+    epochs = []
+    train_loss = []
+    val_loss = []
+
+    with open("log/training_local_base_enhanced_v1.log", "r") as f:
+        for line in f:
+            if "Train time" in line:
+                epoch = int(re.search(r"Epoch (\d+)", line).group(1))
+                loss = float(re.search(r"Loss:\s*([0-9.]+)", line).group(1))
+                epochs.append(epoch)
+                train_loss.append(loss)
+
+            elif "Validation accuracy" in line:
+                loss = float(re.search(r"Loss:([0-9.]+)", line).group(1))
+                val_loss.append(loss)
+    return epochs, train_loss, val_loss
+
 
 file_name = "log/training_base_no_weights.log"
 
-with open("log/training_local_base_enhanced_v1.log", "r") as f:
-    for line in f:
-        if "Train time" in line:
-            epoch = int(re.search(r"Epoch (\d+)", line).group(1))
-            loss = float(re.search(r"Loss:\s*([0-9.]+)", line).group(1))
-            epochs.append(epoch)
-            train_loss.append(loss)
-
-        elif "Validation accuracy" in line:
-            loss = float(re.search(r"Loss:([0-9.]+)", line).group(1))
-            val_loss.append(loss)
-
+epochs, train_loss, val_loss = parse_log(file_name)
 # Define max of Epochs
 MAX_EPOCH = 200
 
