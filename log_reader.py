131a525fe6823ec6c7ea0bc8227ae5ed3792965a
aaron <asvaaron@gmail.com>
2026-02-10 20:23:03 -0600

    Changes in the log reader
    Add script for parsing Epoch, Loss and acurracy in the same  parese_log function

--- log_reader.py
+++ log_reader.py
@@ -2,44 +2,70 @@ import re
 import matplotlib.pyplot as plt
 
 
-
-
-
 def parse_log(log_file):
 
     epochs = []
     train_loss = []
     val_loss = []
+    train_acc = []
+    val_acc = []
 
-    with open("log/training_local_base_enhanced_v1.log", "r") as f:
+    with open(log_file, "r") as f:
         for line in f:
             if "Train time" in line:
                 epoch = int(re.search(r"Epoch (\d+)", line).group(1))
                 loss = float(re.search(r"Loss:\s*([0-9.]+)", line).group(1))
+                acc = re.search(r"Training accuracy:([0-9.]+)", line).group(1)
+                acc = float(acc.rstrip("."))
+
                 epochs.append(epoch)
                 train_loss.append(loss)
+                train_acc.append(acc)
+
 
             elif "Validation accuracy" in line:
                 loss = float(re.search(r"Loss:([0-9.]+)", line).group(1))
+                acc = re.search(r"Validation accuracy:([0-9.]+)", line).group(1)
+                acc = float(acc.rstrip(","))
+
                 val_loss.append(loss)
-    return epochs, train_loss, val_loss
+                train_acc.append(acc)
+
+    return epochs, train_loss, val_loss, train_acc, val_acc
 
 
-file_name = "log/training_base_no_weights.log"
+file_name = "log/training_large_no_weights.log"
 
-epochs, train_loss, val_loss = parse_log(file_name)
+epochs1, train_loss1, val_loss1, train_acc1, val_acc1 = parse_log(file_name)
+
+file_name2 = "log/training_local_large_enhanced_v1.log"
+
+epochs2, train_loss2, val_loss2, train_acc2, val_acc2 = parse_log(file_name2)
+
 # Define max of Epochs
 MAX_EPOCH = 200
 
-filtered_epochs = [e for e in epochs if e <= MAX_EPOCH]
+filtered_epochs = [e for e in epochs1 if e <= MAX_EPOCH]
 idx = len(filtered_epochs)
+# Training Loss
 
 # Plot file
 plt.figure()
-plt.plot(epochs[:idx], train_loss[:idx], label="Train Loss")
-plt.plot(epochs[:idx], val_loss[:idx], label="Validation Loss")
+plt.plot(epochs1[:idx], train_loss1[:idx], label="Train Loss No Changes")
+plt.plot(epochs2[:idx], train_loss2[:idx], label="Train Loss Changes")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-plt.title("Loss per Epoch")
+plt.title("Loss per Epoch large")
+plt.legend()
+plt.show()
+
+# Validation Loss
+
+plt.figure()
+plt.plot(epochs1[:idx], val_loss1[:idx], label="Validation Loss No Changes")
+plt.plot(epochs2[:idx], val_loss2[:idx], label="Validation Loss Changes")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Loss per Epoch large")
 plt.legend()
 plt.show()
