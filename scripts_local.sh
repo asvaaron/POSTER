7e2130c98a8083ff7fb320f905643f028a732636
aaron <asvaaron@gmail.com>
2026-04-09 12:05:07 -0600

    Fix scripts_local.sh
    Change checkpoint for the loading models

--- scripts_local.sh
+++ scripts_local.sh
@@ -30,11 +30,11 @@ python mcnemar_test.py \
   --gpu 0,1 \
   --dataset rafdb \
   --checkpoints \
-    checkpoint/training_small_no_changes/epoch187_acc0.9237.pth \
-    checkpoint/local_small_enhanced_v1/epoch191_acc0.9234.pth\
-    checkpoint/training_base_no_changes/epoch97_acc0.9244.pth \
+    checkpoint/local_small_simple/epoch179_acc0.9237.pth \
+    checkpoint/local_small_enhanced_v1/epoch191_acc0.9234.pth \
+    checkpoint/local_base_simple/epoch222_acc0.9276.pth \
     checkpoint/local_base_enhanced_v1/epoch219_acc0.9247.pth \
-    checkpoint/training_large_no_changes/epoch205_acc0.9241.pth \
+    checkpoint/local_large_simple/epoch200_acc0.925.pth \
     checkpoint/local_large_enchanced_v1/epoch187_acc0.9254.pth \
   --model_names small_baseline small_modified base_baseline base_modified large_baseline large_modified \
   --model_types small small base base large large \
